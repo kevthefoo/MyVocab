b0aa17786da346a79d4ce5f250b95bedf34e77ab
KevTheFoo <kevthefoo@gmail.com>
2024-10-30 17:40:09 +0800

    Add user authentication and dashboard routing
    - Implemented user redirection to the dashboard upon sign-in in the Home component.
    - Created a new Dashboard component that restricts access based on user authentication status.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Dashboard() {
+    const { isSignedIn, isLoaded } = useUser();
+
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!isSignedIn || !isLoaded) {
+            router.push("/");
+        }
+    }, [isSignedIn, router, isLoaded]);
+
+    if (!isLoaded) {
+        // Handle loading state however you like
+        return null;
+    }
+
+    if (!isSignedIn) {
+        return null; // Return null while redirecting
+    }
+
+    return <div>This is a Dashboard</div>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
-import Image from "next/image";
+"use client";
+
+import Link from "next/link";
+
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-    return <div></div>;
+    const { isSignedIn } = useUser();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (isSignedIn) {
+            router.push("/dashboard");
+        }
+    }, [isSignedIn, router]);
+
+    return (
+        <div>
+            <Link href="/dashboard">Dashboard</Link>
+        </div>
+    );
 }
